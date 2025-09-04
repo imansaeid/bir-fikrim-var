f112a32d4b5dcb3a54287fa8a2e7b13b0e246d27
imans <iman.saeid04@gmail.com>
2025-09-04 22:36:52 +0300

    idea controller created

---
+++ bir fikrim var/Controllers/IdeasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using bir_fikrim_var.Models;
+
+namespace bir_fikrim_var.Controllers
+{
+    public class IdeasController : Controller
+    {
+        private readonly MYDBCONTXT _context;
+
+        public IdeasController(MYDBCONTXT context)
+        {
+            _context = context;
+        }
+
+        // GET: Ideas
+        public async Task<IActionResult> Index()
+        {
+            var mYDBCONTXT = _context.Ideas.Include(i => i.User);
+            return View(await mYDBCONTXT.ToListAsync());
+        }
+
+        // GET: Ideas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var idea = await _context.Ideas
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.IdeaId == id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+
+            return View(idea);
+        }
+
+        // GET: Ideas/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Ideas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdeaId,UserId,Title,Content,CreatedDate,LikeCount")] Idea idea)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(idea);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", idea.UserId);
+            return View(idea);
+        }
+
+        // GET: Ideas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var idea = await _context.Ideas.FindAsync(id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", idea.UserId);
+            return View(idea);
+        }
+
+        // POST: Ideas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdeaId,UserId,Title,Content,CreatedDate,LikeCount")] Idea idea)
+        {
+            if (id != idea.IdeaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(idea);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IdeaExists(idea.IdeaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", idea.UserId);
+            return View(idea);
+        }
+
+        // GET: Ideas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var idea = await _context.Ideas
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.IdeaId == id);
+            if (idea == null)
+            {
+                return NotFound();
+            }
+
+            return View(idea);
+        }
+
+        // POST: Ideas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var idea = await _context.Ideas.FindAsync(id);
+            if (idea != null)
+            {
+                _context.Ideas.Remove(idea);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IdeaExists(int id)
+        {
+            return _context.Ideas.Any(e => e.IdeaId == id);
+        }
+    }
+}
