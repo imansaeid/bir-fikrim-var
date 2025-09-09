c3584c929c4a2207282df7ea59d63a6ff1432a04
imans <iman.saeid04@gmail.com>
2025-09-09 04:34:25 +0300

    comment controller completed

--- bir fikrim var/Controllers/CommentsController.cs
+++ bir fikrim var/Controllers/CommentsController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using bir_fikrim_var.Models;
+using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using bir_fikrim_var.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace bir_fikrim_var.Controllers
 {
@@ -29,58 +30,66 @@ namespace bir_fikrim_var.Controllers
 
         // GET: api/Comments/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Comment>> GetComment(int id)
+        public async Task<ActionResult<CommentDto>> GetComment(int id)
         {
+            // 1. Veritabanından primary key ile Comment entity'sini asenkron olarak getir
             var comment = await _context.Comments.FindAsync(id);
 
+            // 2. Eğer böyle bir kayıt yoksa 404 Not Found dön
             if (comment == null)
-            {
                 return NotFound();
-            }
 
-            return comment;
+            // 3. Mapster ile Comment entity'sini CommentDto'ya dönüştür
+            var dto = comment.Adapt<CommentDto>();
+
+            // 4. 200 OK ile DTO'yu JSON olarak client'a gönder
+            return Ok(dto);
         }
+
 
         // PUT: api/Comments/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutComment(int id, Comment comment)
+        public async Task<IActionResult> PutComment(int id, CreateCommentDTO updateDto)
         {
-            if (id != comment.CommentId)
-            {
-                return BadRequest();
-            }
+            // 1. Veritabanından güncellenecek Comment entity'sini getir
+            var existingComment = await _context.Comments.FindAsync(id);
 
-            _context.Entry(comment).State = EntityState.Modified;
+            // 2. Eğer böyle bir kayıt yoksa 404 Not Found dön
+            if (existingComment == null)
+                return NotFound();
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CommentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            // 3. Mapster ile DTO'daki alanları mevcut Comment entity'sine kopyala
+            updateDto.Adapt(existingComment);
+
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
+
+
+
+
         // POST: api/Comments
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Comment>> PostComment(Comment comment)
+        public async Task<ActionResult<CommentDto>> PostComment(CreateCommentDTO createDto)
         {
+            // 1. Mapster ile CreateCommentDTO'dan Comment entity'si oluştur
+            var comment = createDto.Adapt<Comment>();
+
+            // 2. Veritabanına yeni Comment entity'sini ekle
             _context.Comments.Add(comment);
+
+            // 3. Değişiklikleri veritabanına kaydet
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetComment", new { id = comment.CommentId }, comment);
+            // 4. Kaydedilen entity'yi CommentDto'ya dönüştür ve client'a döndür
+            var dto = comment.Adapt<CommentDto>();
+
+            // 5. 201 Created ile birlikte kaydın detaylarını döndür
+            return CreatedAtAction("GetComment", new { id = comment.CommentId }, dto);
         }
 
         // DELETE: api/Comments/5
--- bir fikrim var/Controllers/LikesController.cs
+++ bir fikrim var/Controllers/LikesController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using bir_fikrim_var.Models;
+using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using bir_fikrim_var.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace bir_fikrim_var.Controllers
 {
@@ -22,23 +23,35 @@ namespace bir_fikrim_var.Controllers
 
         // GET: api/Likes
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Like>>> GetLikes()
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetLikes()
         {
-            return await _context.Likes.ToListAsync();
+            // 1. Veritabanındaki tüm Like kayıtlarını asenkron olarak al
+            var likes = await _context.Likes.ToListAsync();
+
+            // 2. Mapster ile entity listesini DTO listesine dönüştür
+            var dtoList = likes.Adapt<List<LikeDto>>();
+
+            // 3. DTO listesini 200 OK ile döndür
+            return Ok(dtoList);
         }
 
         // GET: api/Likes/5
+
         [HttpGet("{id}")]
-        public async Task<ActionResult<Like>> GetLike(int id)
+        public async Task<ActionResult<LikeDto>> GetLike(int id)
         {
+            // 1. Veritabanından primary key ile Like entity'sini asenkron olarak getir
             var like = await _context.Likes.FindAsync(id);
 
+            // 2. Eğer böyle bir kayıt yoksa 404 Not Found dön
             if (like == null)
-            {
                 return NotFound();
-            }
 
-            return like;
+            // 3. Mapster ile Like entity'sini LikeDto'ya dönüştür
+            var dto = like.Adapt<LikeDto>();
+
+            // 4. 200 OK ile DTO'yu JSON olarak client'a gönder
+            return Ok(dto);
         }
 
         // PUT: api/Likes/5
